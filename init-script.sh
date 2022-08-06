a39f417088dd2d18ba70309bf7109da961bbc642
chris <chris@lyonsgroup.family>
2022-08-06 17:43:05 -0400

    updating for docker build arguments

--- init-script.sh
+++ init-script.sh
@@ -2,4 +2,4 @@
 
 echo "Downloading latest CentOS Stream 8 iso"
 curl -L -o CentOS-Stream.iso http://isoredirect.centos.org/centos/8-stream/isos/x86_64/CentOS-Stream-8-x86_64-"$1"-boot.iso
-$(sha256sum /root/CentOS-Stream.iso)
+sha256sum /root/CentOS-Stream.iso
